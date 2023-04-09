8cd3f9430df1ed3d146352c548dfa82d4debfb29
akari kanehira <akak1244g@gmail.com>
2023-04-09 14:13:39 +0900

    SNS認証途中

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '3.0.4'
+ruby '3.0.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.7'
@@ -59,3 +59,7 @@ gem 'cloudinary'
 gem 'dotenv-rails'
 gem 'devise'
 gem 'kaminari'
+
+gem 'omniauth-facebook'
+gem 'omniauth-twitter'
+gem 'omniauth-google-oauth2'
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  devise :omniauthable, omniauth_providers: %i[facebook twitter google_oauth2]
+
   has_many :sakes, dependent: :destroy
 
   validates :name, presence: true 
@@ -15,4 +17,12 @@ class User < ApplicationRecord
     self.likes.exists?(sake_id: sake.id)
   end
   
+<!-- SNS認証 -->
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
+
 end
