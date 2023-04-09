0a6686b764ade67973faadd6c6a0654b2fc4fda4
akari kanehira <akak1244g@gmail.com>
2023-04-09 18:02:50 +0900

    SNS戻した

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable,
   
-          :omniauthable, omniauth_providers: %i[facebook twitter google_oauth2]
 
   has_many :sakes, dependent: :destroy
 
@@ -17,13 +16,6 @@ class User < ApplicationRecord
     self.likes.exists?(sake_id: sake.id)
   end
   
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.name = auth.info.name
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-    end
-  end
 
 end
 
