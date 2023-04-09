f71b66009cfdeae337daa367601298e6f6660310
Tomo0626 <wksrs0626@gmail.com>
2023-04-09 19:26:13 +0900

    ちょびっと変えた

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,10 @@ Rails.application.routes.draw do
   resources :sakes do
     resources :likes, only: [:create, :destroy]
   end
-  get 'sakes/top' => 'sakes#top'
-
   resources :perfumes
 
   resources :tags do
     get 'sakes', to: 'sakes#search'
   end
-
-  root "sakes#top"
+  root "hello#index"
 end
